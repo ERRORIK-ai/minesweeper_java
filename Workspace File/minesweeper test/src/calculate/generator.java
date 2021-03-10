23c652e1fb8b2af7c271000253bd02721a04061e
Douglas <erik.steinacher@gmail.com>
2021-03-10 15:25:12 +0100

    asd

--- Workspace File/minesweeper test/src/calculate/generator.java
+++ Workspace File/minesweeper test/src/calculate/generator.java
@@ -1,9 +1,30 @@
 package calculate;
 
+import java.util.Random;
+
 public class generator {
 
-	public generator() {
-		// TODO Auto-generated constructor stub
+	public void main() {
+	}
+
+	public generator(int width, int height, int percent_mines, int mines) {
+
+		int[][] field = new int[width][height];
+
+		// If Set PERCENTAGE
+		for (int x = 0; x <= width; x++) {
+			for (int y = 0; y <= height; y++) {
+				if (Math.random() < percent_mines / 10) {
+					field[x][y] = 9;
+				}
+			}
+		}
+
+		// IF SET COUNT OF MINES
+
+		int rand_number = Random.nextInt(1 - 0 + 1) + 0;
+				if (rand_number=1) {}
+
 	}
 
 }
--- Workspace File/minesweeper test/src/start/start.java
+++ Workspace File/minesweeper test/src/start/start.java
@@ -5,6 +5,7 @@ public class start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+
 	}
 
 }
