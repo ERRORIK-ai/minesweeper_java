e67f51cc65118f4c7121f6b00648fa8beee7af01
Douglas <erik.steinacher@gmail.com>
2021-03-10 14:39:10 +0100

    sss

--- Workspace File/minesweeper test/src/Settings.java
+++ Workspace File/minesweeper test/src/Settings.java
@@ -26,7 +26,6 @@ import javax.swing.JScrollPane;
 
 public class Settings extends JFrame implements ActionListener
 {
-//asudhiashd
 	private JPanel contentPane;
 	private JTextField txtWidth;
 	private JTextField txtHeight;
