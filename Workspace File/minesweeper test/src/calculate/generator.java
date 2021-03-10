6a4fad64e7563083078bc6de91497e79e10850b6
Douglas <erik.steinacher@gmail.com>
2021-03-10 14:47:15 +0100

    added gen/start class&package

---
+++ Workspace File/minesweeper test/src/calculate/generator.java
@@ -0,0 +1,9 @@
+package calculate;
+
+public class generator {
+
+	public generator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Workspace File/minesweeper test/src/start/start.java
@@ -0,0 +1,10 @@
+package start;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
