b05f8446c3d3298ba037c4998e42de6d315f599b
Douglas <erik.steinacher@gmail.com>
2021-03-11 10:43:46 +0100

    updated generator and start

--- Workspace File/minesweeper test/src/calculate/generator.java
+++ Workspace File/minesweeper test/src/calculate/generator.java
@@ -4,27 +4,50 @@ import java.util.Random;
 
 public class generator {
 
-	public void main() {
-	}
-
-	public generator(int width, int height, int percent_mines, int mines) {
+	public static int[][] generator(int width, int height, double percent_mines, double mines) {
 
 		int[][] field = new int[width][height];
-
-		// If Set PERCENTAGE
-		for (int x = 0; x <= width; x++) {
-			for (int y = 0; y <= height; y++) {
-				if (Math.random() < percent_mines / 10) {
-					field[x][y] = 9;
+		if (mines == 0) {
+			// If Set PERCENTAGE
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					double asfddas = Math.random();
+					if (asfddas < percent_mines / 100) {
+						field[x][y] = 9;
+					}
+				}
+			}
+		} else if (percent_mines == 0) {
+			// IF SET COUNT OF MINES
+			int count_mines = 0;
+			for (; count_mines < mines;) {
+				for (int x = 0; x < width; x++) {
+					for (int y = 0; y < height; y++) {
+						if (Math.random() < mines / (width * height) && count_mines < mines && field[x][y] != 9) {
+							field[x][y] = 9;
+							count_mines++;
+						}
+					}
 				}
 			}
 		}
+		return field;
+	}
 
-		// IF SET COUNT OF MINES
+	public static int[][] calc(int[][] field) {
+		int check_num_x;
+		int check_num_y;
+		
+		int m = field.length;
+        int n = field[0].length;
+        int[][] C = new int[n][m];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                C[j][i] = A[i][j];
+        return C;
+    }
 
-		int rand_number = Random.nextInt(1 - 0 + 1) + 0;
-				if (rand_number=1) {}
-
+		return field;
 	}
 
 }
--- Workspace File/minesweeper test/src/start/start.java
+++ Workspace File/minesweeper test/src/start/start.java
@@ -1,11 +1,39 @@
 package start;
 
+import calculate.*;
+import gui.*;
+
 public class start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-
+		int[][] generator_obj;
+		
+		int width_x=10;
+		int height_y=10;
+		
+		//Eines muss Null sein!
+		//prozent minen max 100 / min 1
+		int prozent_minen=0;
+		//anzhal minen darf nicht mehr als (width_x*height_y) sein
+		int anzahl_minen=10;
+		
+		
+		
+		// width_x, height_y, prozent_minen, anzahl_minen
+		generator_obj = generator.generator(width_x, height_y, prozent_minen, anzahl_minen);
+		
+		
+		
+		//output printer (löschbar)--
+		for (int x = 0; x < width_x; x++) {
+			for (int y = 0; y < height_y; y++) {
+				System.out.print(generator_obj[x][y]);
+			}
+			System.out.println();
+		}
+		//----------------------------
+		
+		
 	}
 
 }
